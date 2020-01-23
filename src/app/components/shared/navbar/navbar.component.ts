0e642a30a6008e3ac642b8e8f919e89f703f854f
Cristopher Estrada <crissin21_01@hotmail.com>
2020-01-22 18:37:42 -0600

    footer and navbar created

---
+++ src/app/components/shared/navbar/navbar.component.ts
@@ -0,0 +1,39 @@
+import { Component, OnInit } from '@angular/core';
+import {Router} from '@angular/router';
+@Component({
+  selector: 'app-navbar',
+  templateUrl: './navbar.component.html',
+  styleUrls: ['./navbar.component.css']
+})
+export class NavbarComponent implements OnInit {
+
+  user;
+  isAdmin = false;
+  isDoctor = false;
+  tipoUsuario = sessionStorage.getItem('tipoUsuario');
+  constructor(private router : Router) { }
+
+  ngOnInit() {
+    this.user = false;
+    if(sessionStorage.getItem('usuario')!=null){
+      this.user = true;
+    }
+
+    if(this.tipoUsuario==="1"){
+      this.isDoctor = true;
+    }
+    else if(this.tipoUsuario=="3"){
+      this.isAdmin = true;
+    }
+    console.log(this.isDoctor);
+    console.log(sessionStorage.getItem('usuario'));
+  }
+
+  logout(){
+    sessionStorage.clear();
+    localStorage.setItem('action','logout');
+    window.location.reload();
+    this.router.navigate(['/home']);
+  }
+
+}
